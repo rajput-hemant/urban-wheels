fbb461240a78d1b95bd80515999d2796a9732fac
rajput-hemant <rajput.hemant2001@gmail.com>
2023-12-02 22:59:29 +0530

    fix: metadata base absolute url for diff deployment contexts
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,16 +21,32 @@ export const cn = (...inputs: ClassValue[]) => {
  * @returns The absolute url for the given path
  */
 export function absoluteUrl(path: string) {
-  switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
-    case "production":
-      return `${siteConfig.url}${path}`;
+  if (process.env.VERCEL) {
+    switch (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+      case "production":
+        return `${siteConfig.url}${path}`;
 
-    case "preview":
-      return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}${path}`;
+      case "preview":
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}${path}`;
 
-    default:
-      // development
-      return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+      default:
+        // development
+        return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+    }
+  } else if (process.env.NETLIFY) {
+    switch (process.env.CONTEXT) {
+      case "production":
+        return `${siteConfig.url}${path}`;
+
+      case "deploy-preview" || "branch-deploy":
+        return `https://${process.env.DEPLOY_PRIME_URL}${path}`;
+
+      default:
+        // development
+        return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+    }
+  } else {
+    return `${siteConfig.url}${path}`;
   }
 }
 
