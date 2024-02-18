2a8a03121efff9fcc237e903e8df05c7f97d1395
rajput-hemant <rajput.hemant2001@gmail.com>
2024-02-18 12:46:13 +0530

    fix: env validation schema
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,7 +6,7 @@ export const env = createEnv({
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
    */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  skipValidation: process.env.SKIP_ENV_VALIDATION === "true",
 
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
@@ -22,20 +22,15 @@ export const env = createEnv({
       .default("development"),
 
     /* -----------------------------------------------------------------------------------------------
-     * NextAuth.js
+     * Kinde Auth
      * -----------------------------------------------------------------------------------------------*/
 
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production" ?
-        z.string()
-      : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url()
-    ),
+    KINDE_CLIENT_ID: z.string(),
+    KINDE_CLIENT_SECRET: z.string(),
+    KINDE_ISSUER_URL: z.string().url(),
+    KINDE_SITE_URL: z.string().default("http://localhost:3000"),
+    KINDE_POST_LOGOUT_REDIRECT_URL: z.string().default("http://localhost:3000"),
+    KINDE_POST_LOGIN_REDIRECT_URL: z.string().default("http://localhost:3000"),
 
     /* -----------------------------------------------------------------------------------------------
      * Google OAuth
