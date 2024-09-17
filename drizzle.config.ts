89dc5e2e145acbf78e9c7d2927481ed20ce4898e
rajput-hemant <rajput.hemant2001@gmail.com>
2024-09-17 18:56:27 +0530

    chore(deps): update all non-major dependencies
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,24 @@
+/* eslint-disable no-restricted-properties */
+import { cwd } from "process";
+import { loadEnvConfig } from "@next/env";
+
 import type { Config } from "drizzle-kit";
 
 import { siteConfig } from "@/config/site";
-import { env } from "@/lib/env";
+
+loadEnvConfig(cwd());
+
+if (!process.env.DATABASE_URL) {
+  console.error("'DATABASE_URL' is not set in the environment variables");
+  process.exit(1);
+}
 
 export default {
   schema: "./src/lib/db/schema",
   out: "./src/lib/db/migrations",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
   tablesFilter: [`${siteConfig.name.toLowerCase().replace(/\s/g, "_")}_*`],
 } satisfies Config;
